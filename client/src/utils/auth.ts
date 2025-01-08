734adb49ac8317b6ee937fb8be85cc58484a3e75
kavue <kvue06@gmail.com>
2025-01-08 17:23:38 -0600

    Update auth.ts

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,29 +1,52 @@
 import { JwtPayload, jwtDecode } from 'jwt-decode';
+import { UserData } from '../interfaces/UserData';
 
 class AuthService {
-  getProfile() {
+  getProfile(): UserData {
     // TODO: return the decoded token
+    return jwtDecode<UserData>(this.getToken());
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const decodedToken = jwtDecode<JwtPayload>(token);
+      
+      if (decodedToken?.exp && decodedToken?.exp < Date.now() / 1000) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.log('Token expired check failed:', error);
+      return false;
+    }
   }
 
   getToken(): string {
     // TODO: return the token
+    const currentUser = localStorage.getItem('id_token') || '';
+    return currentUser;
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem('id_token', idToken);
+    const userInfo = JSON.stringify(jwtDecode<UserData>(idToken));
+    localStorage.setItem('user_info', userInfo);
     // TODO: redirect to the home page
   }
 
   logout() {
     // TODO: remove the token from localStorage
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('user_info');
     // TODO: redirect to the login page
   }
 }
